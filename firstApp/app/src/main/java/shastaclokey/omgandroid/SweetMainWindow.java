b8567e44ffd9fa06ab2914fae5b88c2018bdb486
shastaclokey1 <shasta.clokey@vmuv.io>
2018-01-12 14:42:46 -0800

    made a fun little "hello world" android app. haha nothing special

---
+++ firstApp/app/src/main/java/shastaclokey/omgandroid/SweetMainWindow.java
@@ -0,0 +1,14 @@
+package shastaclokey.omgandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SweetMainWindow extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sweet_main_window);
+    }
+}
