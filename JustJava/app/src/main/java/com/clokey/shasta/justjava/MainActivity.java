dd61a625d80b94ae2a172c44bf6153d517a75627
shastaclokey1 <shasta.clokey@vmuv.io>
2018-01-17 15:45:19 -0800

    made an app that lets users order coffee

---
+++ JustJava/app/src/main/java/com/clokey/shasta/justjava/MainActivity.java
@@ -0,0 +1,81 @@
+/**
+ * IMPORTANT: Make sure you are using the correct package name. 
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+package com.clokey.shasta.justjava;
+
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity
+{
+    int numCoffees = 0;
+    int coffeeUnitPrice = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view)
+    {
+        displayPrice(numCoffees * coffeeUnitPrice);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number)
+    {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText("Total: " + NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method adds a coffee to the order.
+     */
+    public void addCoffeeToOrder(View view)
+    {
+        numCoffees++;
+        display(numCoffees);
+    }
+
+    /**
+     * This method adds a coffee to the order.
+     */
+    public void subtractCoffeeFromOrder(View view)
+    {
+        numCoffees--;
+        if (numCoffees < 0) //logic to ensure you can't order a negative number of coffees
+            numCoffees = 0;
+        display(numCoffees);
+    }
+
+
+}
