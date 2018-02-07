fc465b5f690c701232f73b61b69d6aafc01cf852
shastaclokey1 <shasta.clokey@vmuv.io>
2018-02-07 11:13:12 -0800

    created an async task to make the http request to load the earthquake data from the internet updated the utils class to include functions designed to make http requests, connect to a server, and pull down json data about earthquakes that can be parsed and handled

--- ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -19,12 +19,14 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -32,6 +34,8 @@ public class EarthquakeActivity extends AppCompatActivity
 {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    /** URL for earthquake data from the USGS dataset */
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=20";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -39,9 +43,53 @@ public class EarthquakeActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        // Create an earthquake data fetcher asyc task and tell it to go get data from the usgs website at the url we specify
+        EarthquakeDataFetcher asyncDataFetcher = new EarthquakeDataFetcher();
+        asyncDataFetcher.execute(USGS_REQUEST_URL);
+    }
 
+    //2)Define a private async task implementing class within the main activity which will use the QueryUtils
+    //  static methods to fetch data form the usgs database
+    //private async task class that runs our earthquake data fetching in the background so not to slow down the UI
+    private class EarthquakeDataFetcher extends AsyncTask<String, Void, ArrayList<Earthquake>>
+    {
+
+        //3)Override the do in background task to call the necessary http networking funcitons
+        @Override
+        protected ArrayList<Earthquake> doInBackground(String... urls)
+        {
+            //check for null urls and for the case when no urls get passed in
+            if (urls.length < 1 || urls[0] == null)
+                return null;
+
+            //otherwise start the async http request for earthquake data
+            ArrayList<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(urls[0]);
+            return earthquakes;
+        }
+
+        //5)Override the onPostExecute method to call the update UI method
+        /**
+         * Update the screen with the given earthquake (which was the result of the
+         * {@link EarthquakeDataFetcher}).
+         */
+        @Override
+        protected void onPostExecute(ArrayList<Earthquake> earthquakes)
+        {
+            if (earthquakes.size() < 1 || earthquakes == null)
+                return;
+
+            updateUi(earthquakes);
+        }
+    }
+
+
+    //4)Make a private void "update ui" method that finds the list view for the main activity, creates a custom
+    //  adapter to display the list items, and sets the item on click listener for the list view items
+    /**
+     * Update the UI with the given earthquake information.
+     */
+    private void updateUi(ArrayList<Earthquake> earthquakes)
+    {
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
@@ -65,4 +113,6 @@ public class EarthquakeActivity extends AppCompatActivity
             }
         });
     }
+
+
 }
